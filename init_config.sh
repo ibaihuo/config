64093af1881c741efb4987a0435962869c7a8edb
renewjoy <oyea9le@qq.com>
2015-08-11 16:25:09 +0800

    update init_config

--- init_config.sh
+++ init_config.sh
@@ -2,13 +2,13 @@
 ######################################################################
 ## Filename:      init_config.sh
 ##                
-## Copyright (C) 2013,  renewjoy
+## Copyright (C) 2013, 2015,  renewjoy
 ## Version:       
 ## Author:        renewjoy <oyea9le@gmail.com>
 ## Created at:    Sat Jun 15 22:51:46 2013
 ##                
 ## Modified by:   renewjoy <oyea9le@gmail.com>
-## Modified at:   Sun Jul  7 21:34:11 2013
+## Modified at:   Tue Aug 11 16:23:24 2015
 ## Description:   初始化配置文件
 ##                
 ######################################################################
@@ -24,26 +24,26 @@ ln -s ${working_dir}/emacs/emacs ~/.emacs
 ln -s ${working_dir}/emacs/emacs.d ~/.emacs.d
 
 # qtile
-rm -rf ~/.config/qtile/config.py
-ln -s ${working_dir}/qtile/config.py ~/.config/qtile/config.py
+# rm -rf ~/.config/qtile/config.py
+# ln -s ${working_dir}/qtile/config.py ~/.config/qtile/config.py
 
 # i3 
-rm -rf ~/.i3/config
-ln -s ${working_dir}/i3/config ~/.i3/config
+# rm -rf ~/.i3/config
+# ln -s ${working_dir}/i3/config ~/.i3/config
 
 # zsh
-rm -rf ~/.zshrc ~/.oh-my-zsh
-ln -s ${working_dir}/zsh/zshrc ~/.zshrc
-ln -s ${working_dir}/zsh/oh-my-zsh ~/.oh-my-zsh
+# rm -rf ~/.zshrc ~/.oh-my-zsh
+# ln -s ${working_dir}/zsh/zshrc ~/.zshrc
+# ln -s ${working_dir}/zsh/oh-my-zsh ~/.oh-my-zsh
 
 # git
-rm -rf ~/.gitconfig
-ln -s ${working_dir}/git/gitconfig ~/.gitconfig
+# rm -rf ~/.gitconfig
+# ln -s ${working_dir}/git/gitconfig ~/.gitconfig
 
 
 # make.conf and portage
-sudo rm -rf /etc/portage/make.conf
-sudo ln -s ${working_dir}/funtoo/make.conf /etc/portage/make.conf
+# sudo rm -rf /etc/portage/make.conf
+# sudo ln -s ${working_dir}/funtoo/make.conf /etc/portage/make.conf
 
-sudo rm -rf /etc/portage/package.use
-sudo ln -s ${working_dir}/funtoo/package.use /etc/portage/package.use
+# sudo rm -rf /etc/portage/package.use
+# sudo ln -s ${working_dir}/funtoo/package.use /etc/portage/package.use
